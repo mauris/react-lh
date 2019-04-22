25fc8fbea7dba91f1f104b641426d3a0c2f9e03a
Sam Yong <samyonggit@gmail.com>
2019-04-22 15:53:26 +0800

    add umd build

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+import commonjs from 'rollup-plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
+
+const env = process.env.NODE_ENV;
+
+const config = {
+  input: 'src/index.js',
+  external: Object.keys(pkg.peerDependencies || {}),
+  output: {
+    format: 'umd',
+    name: 'LoudHailer',
+    globals: {
+      react: 'React'
+    }
+  },
+  plugins: [
+    nodeResolve(),
+    babel({
+      exclude: '**/node_modules/**',
+      runtimeHelpers: true
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(env)
+    }),
+    commonjs({
+      namedExports: {}
+    })
+  ]
+};
+
+if (env === 'production') {
+  config.plugins.push(
+    terser({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false
+      }
+    })
+  );
+}
+
+export default config;
