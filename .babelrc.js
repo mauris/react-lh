801d1a914332a05f3169042e61fa319191d19273
Sam Yong <samyonggit@gmail.com>
2019-05-28 20:40:32 +0800

    update babel config

--- .babelrc.js
+++ .babelrc.js
@@ -1,9 +1,15 @@
+const { NODE_ENV, BABEL_ENV } = process.env;
+const cjs = NODE_ENV === 'test' || BABEL_ENV === 'commonjs';
+const loose = true;
 
 module.exports = {
-  presets: ["@babel/react", "@babel/env"],
+  presets: ["@babel/react", ['@babel/env', { loose }]],
   plugins: [
     ['@babel/proposal-decorators', { legacy: true }],
-    ['@babel/proposal-object-rest-spread'],
-    ['@babel/proposal-export-default-from']
-  ],
+    ['@babel/proposal-object-rest-spread', { loose }],
+    '@babel/transform-react-jsx',
+    cjs && ['@babel/transform-modules-commonjs', { loose }],
+    ['@babel/transform-runtime', { useESModules: !cjs }],
+    ['@babel/proposal-export-default-from'],
+  ].filter(Boolean),
 };
