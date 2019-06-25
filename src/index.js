68494779462a3cfce4b2ae33917423ee30482960
Sam Yong <samyonggit@gmail.com>
2019-06-25 22:58:11 +0800

    add useLoudHailer hook implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default, default as wrapper } from './loudHailer';
 export { createChannel } from './channel';
 export { default as CrossWindow } from './crossWindow';
+export { useLoudHailer } from './useLoudHailer';
---
+++ src/useLoudHailer.js
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { createChannel } from './channel';
+
+export default function useLoudHailer(callback, ...args) {
+  if (!useEffect) {
+    throw new Error(`Effect hook not available in the current version of React installed.
+      Please upgrade to at least React v16.8`);
+  }
+  useEffect(() => {
+    // setup
+    const channel = createChannel.apply(null, args);
+
+    // execute callback with channel
+    callback(channel);
+
+    return () => {
+      // clean up
+      channel.unsubscribe();
+    };
+  });
+}
