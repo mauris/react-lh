5551471147588fcdd0a4f95ad892cbf333ac970e
Sam Yong <samyonggit@gmail.com>
2019-06-25 23:14:39 +0800

    fix incorrect export for index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { default, default as wrapper } from './loudHailer';
 export { createChannel } from './channel';
 export { default as CrossWindow } from './crossWindow';
-export { useLoudHailer } from './useLoudHailer';
+export { default as useLoudHailer } from './useLoudHailer';
