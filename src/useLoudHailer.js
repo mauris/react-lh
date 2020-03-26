04193bb469202d7ffa866b6f7d9bb38b3d110872
Sam Yong <samyonggit@gmail.com>
2020-03-26 21:36:26 +0800

    update dependencies

--- src/useLoudHailer.js
+++ src/useLoudHailer.js
@@ -1,14 +1,14 @@
 import { useEffect } from 'react';
 import { createChannel } from './channel';
 
-export default function useLoudHailer(callback, ...args) {
+export default function useLoudHailer(callback, depsArg = undefined) {
   if (!useEffect) {
     throw new Error(`Effect hook not available in the current version of React installed.
       Please upgrade to at least React v16.8`);
   }
   useEffect(() => {
     // setup
-    const channel = createChannel(null);
+    const channel = createChannel();
 
     // execute callback with channel
     callback(channel);
@@ -17,5 +17,5 @@ export default function useLoudHailer(callback, ...args) {
       // clean up
       channel.unsubscribe();
     };
-  }, ...args);
+  }, depsArg);
 }
