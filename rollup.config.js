e6104b299d5396d563685f6ae4c94680928da427
Sam Yong <samyonggit@gmail.com>
2020-03-26 21:36:18 +0800

    update packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
